6012b2c9761050dcfaf5132d4b600c19eb5d5d1f
Igor <zxoxz@mail.ru>
2017-05-09 14:48:08 +0300

    new

--- js/script.js
+++ js/script.js
@@ -4,7 +4,34 @@ var left = document.querySelector('.column-left');
 var right = document.querySelector('.column-right');
 
 
+var width = 1024;
+var position = 0;
+var slideIndex = 0;
+var slides = document.querySelectorAll('.slider-frame .slide');
+slideFrame.insertAdjacentElement("beforeEnd", slides[0].cloneNode(true));
+slideFrame.insertAdjacentElement("afterBegin", slides[slides.length-1].cloneNode(true));
 
+
+left.onclick = function() {
+var t = parseFloat(getComputedStyle(slideFrame).marginLeft);
+slideFrame.style.marginLeft = (t += width) +"px";
+slideIndex --;
+
+if(slideIndex === -1) {
+slideFrame.style.marginLeft = (-width * slides.length) +"px";
+slideIndex = slides.length - 1;
+}	
+}
+
+right.onclick = function() {
+	var t = parseFloat(getComputedStyle(slideFrame).marginLeft);
+slideFrame.style.marginLeft = (t -= width) +"px";	
+slideIndex ++;
+if(slideIndex === slides.length) {
+slideFrame.style.marginLeft = -width +"px";
+slideIndex = 0;
+}
+}
 
 
 
@@ -22,28 +49,30 @@ var right = document.querySelector('.column-right');
 
 
 
+// var width = 1024;
+// var position = 0;
+// var slideIndex = 0;
+// var slides = document.querySelectorAll('.slider-frame .slide');
+// slideFrame.insertAdjacentElement("beforeEnd", slides[0].cloneNode(true));
+// slideFrame.insertAdjacentElement("afterBegin", slides[slides.length-1].cloneNode(true));
 
-var width = 1024;
-var position = 0;
-var slideIndex = 1;
-var slides = document.querySelectorAll('.slider-frame .slide');
-slideFrame.insertAdjacentElement("beforeEnd", slides[0].cloneNode(true));
-slideFrame.insertAdjacentElement("afterBegin", slides[slides.length-1].cloneNode(true));
+
+// left.onclick = function() {
+// position = Math.min(position + width, 0);
+// slideFrame.style.marginLeft = position +"px";
+// slideIndex --;
+
+// if(slideIndex < 1) {
+// 	slideIndex = 1;
+
+// }	
+// }
+
+// right.onclick = function() {
+// 	slideIndex+=1;
+// position = Math.max(position - width, -width*(slides.length-1))	
+// slideFrame.style.marginLeft = position +"px";	
+
+// }
 
 
-left.onclick = function() {
-slideIndex -= 1;
-if(slideIndex < 1) {
-	slideIndex = 1;
-
-}
-position = Math.min(position + width, 0);
-slideFrame.style.marginLeft = position +"px";	
-}
-
-right.onclick = function() {
-	slideIndex+=1;
-position = Math.max(position - width, -width*(slides.length-1))	
-slideFrame.style.marginLeft = position +"px";	
-
-}
