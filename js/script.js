3874259e272bcca6afa1737ce35f19900062dd79
Igor <zxoxz@mail.ru>
2017-05-11 23:24:46 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,78 +1,52 @@
+$(document).ready(function(){
 
-var slideFrame = document.querySelector('.slider-frame');
-var left = document.querySelector('.column-left');
-var right = document.querySelector('.column-right');
+var run = setInterval('rotate()', 4000);
+
+var slide = $(".slide");
+var slideFrame = $(".slider-frame");
+var width = $(".slide").outerWidth();
+var leftShift = width * -1;
+
+var left = $('.column-left');
+var right = $('.column-right');
+
+$(".slide:first").before($(".slide:last"));
+$(".slider-frame").css({left:leftShift});
+
+left.click(function(){
+var moveSize = parseInt(slideFrame.css("left")) - leftShift;
+$(".slider-frame:not(:animated)").animate({left:moveSize}, 800, function(){
+
+$(".slide:first").before($(".slide:last"));
+slideFrame.css({left:leftShift});
+
+});
+
+$(".lips:not(:first)").remove();
+$(".lips:first").css({transform:"scale(0)"});
+});
+
+right.click(function(){
+var moveSize = parseInt(slideFrame.css("left")) + leftShift;
+$(".slider-frame:not(:animated)").animate({left:moveSize}, 800, function(){
+
+$(".slide:last").after($(".slide:first"));
+slideFrame.css({left:leftShift});
+});
+$(".lips:not(:first)").remove();
+$(".lips:first").css({transform:"scale(0)"});
+});
 
 
-var width = 1024;
-var position = 0;
-var slideIndex = 0;
-var slides = document.querySelectorAll('.slider-frame .slide');
-slideFrame.insertAdjacentElement("beforeEnd", slides[0].cloneNode(true));
-slideFrame.insertAdjacentElement("afterBegin", slides[slides.length-1].cloneNode(true));
+$(".container").hover(
+function() {clearInterval(run)},
+function() {run = setInterval('rotate()', 2000)}
+	)
 
+}) 
 
-left.onclick = function() {
-var t = parseFloat(getComputedStyle(slideFrame).marginLeft);
-slideFrame.style.marginLeft = (t += width) +"px";
-slideIndex --;
+function rotate() {
 
-if(slideIndex === -1) {
-slideFrame.style.marginLeft = (-width * slides.length) +"px";
-slideIndex = slides.length - 1;
-}	
+$('.column-right').click();
+
 }
-
-right.onclick = function() {
-	var t = parseFloat(getComputedStyle(slideFrame).marginLeft);
-slideFrame.style.marginLeft = (t -= width) +"px";	
-slideIndex ++;
-if(slideIndex === slides.length) {
-slideFrame.style.marginLeft = -width +"px";
-slideIndex = 0;
-}
-}
-
-
-
-// var currentImage = 0;
-// function moveSlide(n) {
-// var slides = document.querySelectorAll('.slider-frame .slide');
-// if(currentImage < 0) currentImage = slides.length-1;
-// if (currentImage > slides.length-1) currentImage = 0;
-// slideFrame.style.marginLeft = slides[0].getBoundingClientRect().left - slides[currentImage].getBoundingClientRect().left + 'px';
-// }
-
-
-
-// slideFrame.style.marginLeft = parseFloat(getComputedStyle(slideFrame).marginLeft) + 1024 +"px";	
-
-
-
-// var width = 1024;
-// var position = 0;
-// var slideIndex = 0;
-// var slides = document.querySelectorAll('.slider-frame .slide');
-// slideFrame.insertAdjacentElement("beforeEnd", slides[0].cloneNode(true));
-// slideFrame.insertAdjacentElement("afterBegin", slides[slides.length-1].cloneNode(true));
-
-
-// left.onclick = function() {
-// position = Math.min(position + width, 0);
-// slideFrame.style.marginLeft = position +"px";
-// slideIndex --;
-
-// if(slideIndex < 1) {
-// 	slideIndex = 1;
-
-// }	
-// }
-
-// right.onclick = function() {
-// 	slideIndex+=1;
-// position = Math.max(position - width, -width*(slides.length-1))	
-// slideFrame.style.marginLeft = position +"px";	
-
-// }
-
-
